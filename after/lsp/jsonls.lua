864f3352376e5a3bbdc9a721221e156abfdfdf22
Kenny Ek <kennyswek@gmail.com>
2026-01-02 09:38:31 +0100

    chore(lsp): explicitly call lsp config in after setups

--- after/lsp/jsonls.lua
+++ after/lsp/jsonls.lua
@@ -1,5 +1,5 @@
-return {
+vim.lsp.config("jsonls", {
 	on_attach = function(client)
 		client.server_capabilities.documentRangeFormattingProvider = false
 	end,
-}
+})
--- after/lsp/vtsls.lua
+++ after/lsp/vtsls.lua
@@ -1,6 +1,6 @@
-return {
+vim.lsp.config("vtsls", {
 	on_attach = function(client)
 		client.server_capabilities.documentFormattingProvider = false
 		client.server_capabilities.documentRangeFormattingProvider = false
 	end,
-}
+})
