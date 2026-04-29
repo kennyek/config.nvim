5856c670bff2a3105e50e0bfe8c9c435ac57a2b0
Kenny Ek <kennyswek@gmail.com>
2026-04-29 21:16:10 +0200

    feat(plugin): render markdown

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,7 @@
+vim.pack.add({
+	"https://github.com/nvim-treesitter/nvim-treesitter",
+	"https://github.com/nvim-tree/nvim-web-devicons",
+	"https://github.com/MeanderingProgrammer/render-markdown.nvim",
+})
+
+require("render-markdown").setup({})
