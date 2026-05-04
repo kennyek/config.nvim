a86e7c0101c463a983d7ad50d6918dc66c44c468
Kenny Ek <kenny.ek@surikat.se>
2026-05-04 12:14:37 +0200

    feat: add plugins for displaying colors and images

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,13 +1,16 @@
 vim.pack.add({
-	"https://github.com/folke/tokyonight.nvim",
+  "https://github.com/folke/tokyonight.nvim",
+  "https://github.com/catgoose/nvim-colorizer.lua",
 })
 
 require("tokyonight").setup({
-	style = "night",
-	transparent = true,
-	styles = {
-		comments = { italic = false },
-	},
+  style = "night",
+  transparent = true,
+  styles = {
+    comments = { italic = false },
+  },
 })
 
 vim.cmd("colorscheme tokyonight-night")
+
+require("colorizer").setup()
